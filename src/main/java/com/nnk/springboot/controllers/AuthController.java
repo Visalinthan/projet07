8b240b7d07601ce14152ba1acfba4f8a26bd456e
Visalinthan <cvisalinthan@yahoo.fr>
2022-03-15 17:56:35 +0100

    token

--- src/main/java/com/nnk/springboot/controllers/AuthController.java
+++ src/main/java/com/nnk/springboot/controllers/AuthController.java
@@ -70,9 +70,7 @@ public class AuthController {
                     .body(new MessageResponse("Error: Email is already in use!"));
         }
         // Create new user's account
-        User user = new User(signUpRequest.getUsername(),
-                signUpRequest.getEmail(),
-                encoder.encode(signUpRequest.getPassword()));
+        User user = new User(signUpRequest.getUsername(),encoder.encode(signUpRequest.getPassword()),signUpRequest.getEmail());
         Set<String> strRoles = signUpRequest.getRole();
         Set<Role> roles = new HashSet<>();
         if (strRoles == null) {
